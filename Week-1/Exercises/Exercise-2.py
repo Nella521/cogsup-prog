f47682b72819eebc8f82191a4c84d2f2d5dc2efa
nella521 <98780991+Nella521@users.noreply.github.com>
2025-10-30 15:36:23 +0100

    exercise 2 done

--- Week-1/Exercises/Exercise-2.py
+++ Week-1/Exercises/Exercise-2.py
@@ -14,12 +14,11 @@ Task:
 Write a for-loop that prints out the following line 20 times:
  All work and no play makes Jack a dull boy.
 """
+print(" Exercise 2.1 ")
 
-print("Exercise 2.1")
+for i in range(1,21):
+  print(f"{i}: ---")
 
-pass
-
-print("---")
 """
 Exercise 2.2
 
@@ -30,10 +29,10 @@ Write a for-loop that prints out the numbers from 0 up to 5 inclusive.
 
 print("Exercise 2.2")
 
-pass
+for i in range(6):
+  print(i)
 
 print("---")
-
 
 """
 Exercise 2.3
@@ -45,7 +44,9 @@ Write a for-loop that prints out the EVEN numbers from 2 up to 8 inclusive.
 
 print("Exercise 2.3")
 
-pass
+for i in range (2, 9):
+  if i%2==0: 
+    print (i)
 
 print("---")
 
@@ -59,7 +60,8 @@ Now write another loop to print 9 through 0 (i.e., backwards).
 
 print("Exercise 2.4")
 
-pass
+for i in range(9, -1, -1):
+  print (i)
 
 print("---")
 
@@ -81,7 +83,9 @@ Write code that prints out the following sequence:
 
 print("Exercise 2.5")
 
-pass
+for i in range (9):
+  print ('z'*i)
+
 
 print("---")
 
@@ -106,7 +110,10 @@ of a simple print-statement.
 
 print("Exercise 2.6")
 
-pass
+lista = []
+for i in range(1, 6):
+  lista.append(i)
+  print(*lista)
 
 print("---")
 
@@ -135,7 +142,12 @@ your code needs to carry out for a certain value of rows.
 print("Exercise 2.7")
 
 rows = 5
-pass
+
+for i in range(1, rows + 1):
+    spaces = rows - i
+    o = 2 * i - 1
+    print(" " * spaces + "o" * o)
+
 
 print("---")
 
@@ -163,6 +175,8 @@ on how to align the numbers.
 
 print("Exercise 2.8")
 
-pass
+for i in range(1, 11):
+    print("".join(f"{i*j:>4}" for j in range(1, 11)))
+
 
 print("---")
